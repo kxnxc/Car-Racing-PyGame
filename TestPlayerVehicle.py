994afef341874f1e3ab8aff87d550bd0c889d795
Yehor Vitvytskyi <grey5393@gmail.com>
2024-05-06 17:13:19 +0300

    fixed tests

--- TestPlayerVehicle.py
+++ TestPlayerVehicle.py
@@ -1,28 +1,31 @@
-import unittest
+import pytest
 import pygame
 from model.PlayerVehicle import PlayerVehicle
 from model.Vehicle import Vehicle
 
-
-class TestPlayerVehicle(unittest.TestCase):
-    def setUp(self):
-        self.test_image = pygame.Surface((100, 100))
-
-    def test_vehicle_creation(self):
-        vehicle = Vehicle(self.test_image, 50, 50)
-        self.assertIsInstance(vehicle, Vehicle)
-        self.assertIsNotNone(vehicle.image)
-        self.assertEqual(vehicle.rect.center, (50, 50))
-
-    def test_player_vehicle_creation(self):
-        player_vehicle = PlayerVehicle(50, 50)
-        self.assertIsInstance(player_vehicle, PlayerVehicle)
-        self.assertIsNotNone(player_vehicle.image)
-        self.assertEqual(player_vehicle.rect.center, (50, 50))
-
-    def tearDown(self):
-        pass
+pygame.init()
 
 
-if __name__ == '__main__':
-    unittest.main()
+@pytest.fixture
+def test_image():
+    return pygame.Surface((100, 100))
+
+
+def test_vehicle_creation(test_image):
+    vehicle = Vehicle(test_image, 50, 50)
+    assert isinstance(vehicle, Vehicle), \
+        "Об'єкт має бути екземпляром класу Vehicle"
+    assert vehicle.image is not None, \
+        "Vehicle повинен мати зображення"
+    assert vehicle.rect.center == (50, 50), \
+        "Координати центру мають бути (50, 50)"
+
+
+def test_player_vehicle_creation(test_image):
+    player_vehicle = PlayerVehicle(50, 50)
+    assert isinstance(player_vehicle, PlayerVehicle), \
+        "Об'єкт повинен бути екземпляром класу PlayerVehicle"
+    assert player_vehicle.image is not None, \
+        "PlayerVehicle повинен мати зображення"
+    assert player_vehicle.rect.center == (50, 50), \
+        "Координати центру мають бути (50, 50)"
